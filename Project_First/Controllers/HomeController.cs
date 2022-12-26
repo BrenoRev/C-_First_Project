c4380dfc159a60200540c54f6fb594a6e7a2c6c9
BrenoRev <silvabreno462@gmail.com>
2022-12-26 00:15:58 -0300

    Layout inicial

--- Project_First/Controllers/HomeController.cs
+++ Project_First/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Project_First.Models;
+using Project_First.Service;
 
 namespace Project_First.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
+    private readonly HomeHelper _homeHelper;
+    
     public HomeController(ILogger<HomeController> logger)
     {
+        _homeHelper = new HomeHelper();
         _logger = logger;
     }
 
     public IActionResult Index()
     {
-        return View();
+        UserModel userModel = new UserModel();
+        
+        userModel.Name = "Breno";
+        userModel.Email = "silvabreno462@gmail.com";
+        const string PASSWORD = "breno@123";
+        
+        _logger.Log(LogLevel.Debug, "Password before encrypt: ${PASSWORD}");
+        userModel.Password = _homeHelper.hashPassword(PASSWORD);
+        _logger.Log(LogLevel.Debug, "Password after encrypt: ${userModel.Password}");
+        
+        return View(userModel);
     }
 
     public IActionResult Privacy()
---
+++ Project_First/Service/HomeHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_First.Service;
+
+public class HomeHelper
+{
+    public string hashPassword(string password)
+    {
+        int passwordLength = password.Length;
+        for(int index = 0; index < passwordLength; index++)
+        {
+            password = password.Replace(password[index], (char)(password[index] + 1));
+        }
+
+        return password;
+    }
+}
